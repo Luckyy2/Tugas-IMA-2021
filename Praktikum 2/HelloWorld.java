620d637e7c8d0c28957031da9e492166ba3607e0
Luckyy2 <bimolp@student.telkomuniversity.ac.id>
2021-09-27 00:27:44 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+ public static void main(String[] args) {
+  System.out.println("NAMA :Bimo Lucky Prasetyo");
+  System.out.println("NIM  :6706213001");
+  System.out.println("KELAS:D3 RPLA 45-01");
+ }
+} 
